3f423dd2481b5792e003572454a3416eebf220f6
Doc Snyder <44336447+R2ToolDroid@users.noreply.github.com>
2018-12-22 22:26:20 +0100

    New Commands
    Add new commands

--- R2MainController.ino
+++ R2MainController.ino
@@ -21,8 +21,9 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 // PCM Ports
 // 0 = Servo Arm rechts auf ab
 // 1 = Servo Hand
-// 3 = Servo Arm Links auf ab
-// 4 =
+// 2 = Servo Arm Links auf ab
+// 3 = Servo Schrauber an/aus
+// 4 = 
 // 6 =
 // 7 =
 // 8 =
@@ -63,6 +64,8 @@ int ServosAus() {
 
 int Sysreset(){
 
+       pwm.setPWM(1, 0, pulseWidth(120));///Gripper ZU
+       delay(500);
        pwm.setPWM(0, 0, pulseWidth(30));///Arm 1 zurück
        delay(200);
        pwm.setPWM(1, 0, pulseWidth(85));///Gripper ZU
@@ -83,11 +86,11 @@ int Sysreset(){
       
       pwm.setPWM(7, 0, pulseWidth(SchubaZu[1]));///Schuba S1
       delay(50);
-      pwm.setPWM(6, 0, pulseWidth(SchubaZu[2]));///Schuba S1
+      pwm.setPWM(6, 0, pulseWidth(SchubaZu[2]));///Schuba S2
       delay(50);
-      pwm.setPWM(5, 0, pulseWidth(SchubaZu[3]));///Schuba S1
+      pwm.setPWM(5, 0, pulseWidth(SchubaZu[3]));///Schuba S3
       delay(50);
-      pwm.setPWM(4, 0, pulseWidth(SchubaZu[4]));///Schuba S1
+      pwm.setPWM(4, 0, pulseWidth(SchubaZu[4]));///Schuba S4
 
       delay(200);
       
@@ -412,61 +415,65 @@ void readCom() {
 
 void parseCommand(String cmd) {
 
-    if (cmd == "test") {
+    if (cmd == "CBD1") {
       if (debug){
-        Serial.println("######Comando######");
+        Serial.println("######Comando CBD1######");
         Serial.println(cmd);
         
       }
-    Serial1.print("5");     // to Coin Levelstatus
-    
-    Serial3.print(":OP09");           // hier geht es weiter zum Marcduino Dome Controller
+      
+    //Serial1.print("5");     // to Coin Levelstatus
+    Serial2.print("mode1");           // hier geht es weiter zum Marcduino Dome Controller
+    Serial2.print('\r');
+    delay(500);
+    Serial3.print(":SE13");           // hier geht es weiter zum Marcduino Dome Controller
     Serial3.print('\r');
-
-    delay(1000);
-    Serial3.print(":CL00");           // hier geht es weiter zum Marcduino Dome Controller
-    Serial3.print('\r');
+    Sysreset();
       
     }
-
-    if (cmd == "try dome") {
+    if (cmd == "CBD2") {
     if (debug){
-        Serial.println("######Comando try dome######");
-        Serial.println(cmd);
-      }
-      Serial2.print("try dome");           // hier geht es weiter zum  Dome Controller
-      Serial2.print('\r');
-
-      Sysreset();
-      
-      for (int thisKlappe = 1; thisKlappe <= 5; thisKlappe++) {
-    
-         int port = thisKlappe +10;
-         pwm.setPWM(port, 0, pulseWidth(KlappeAuf[thisKlappe]));//Klappe x      
-         delay(2000);
-         pwm.setPWM(port, 0, pulseWidth(KlappeZu[thisKlappe]));//Klappe x    
-      }
-
-      CloseAll();
-           
-    }
-
-    if (cmd == "mode1") {
-    if (debug){
-        Serial.println("######Comando Dome Mode 1 RC on ######");
-        Serial.println(cmd);
-      }
-      Serial2.print("mode1");           // hier geht es weiter zum  Dome Controller
-      Serial2.print('\r');
-    }
-  
-    if (cmd == "mode0") {
-    if (debug){
-        Serial.println("######Comando Dome Mode 0 RAND RC off ######");
+        Serial.println("######Comando CBD2 ######");
         Serial.println(cmd);
       }
       Serial2.print("mode0");           // hier geht es weiter zum  Dome Controller
       Serial2.print('\r');
+      delay(500);
+
+      Serial2.print("$28");           // hier geht es weiter zum  Dome Controller
+      Serial2.print('\r');
+      delay(500);
+    }
+    
+    if (cmd == "CBD3" ){
+      if (debug){
+        Serial.println("######Comando Dome CBD3 ######");
+        Serial.println(cmd);
+      }
+      Serial2.print("nono");           // hier geht es weiter zum  Dome Controller
+      Serial2.print('\r');
+      delay(500);
+      Serial3.print("$214");           // hier geht es weiter zum Marcduino Dome Controller
+      Serial3.print('\r');
+      pwm.setPWM(14, 0, pulseWidth(KlappeAuf[4]));//Klappe x  
+      delay(500);
+       
+      
+    }
+  
+    if (cmd == "CB0") {
+    if (debug){
+        Serial.println("######Comando CB0 ######");
+        Serial.println(cmd);
+      }
+      Serial2.print("usb");           // hier geht es weiter zum  Dome Controller
+      Serial2.print('\r');
+      delay(4000);
+      Serial3.print(":OP02");           // hier geht es weiter zum Marcduino Dome Controller
+      Serial3.print('\r');
+      delay(500);
+      Serial3.print("$26");
+      Serial3.print("\r");
     }
 
   
@@ -486,88 +493,273 @@ void parseCommand(String cmd) {
        }
        debug=0;
     }
-    /////USB Komando/////
-    if (cmd == "usb") {
+    ///// Komando/////
+    if (cmd == "CB1") {
     if (debug){
-          Serial.println("######Comando - usb######");
+          Serial.println("######Comando - CB1######");
          Serial.println(cmd);
        }
 
-      Serial3.print(":OP02");
+      
+      Serial2.print("tool1");
+      Serial2.print("\r");
+      delay(4000); 
+      Serial3.print(":OP04");
       Serial3.print("\r");
       delay(100);
-      Serial2.print("usb");
-      Serial2.print("\r");
-      delay(100);
-      pwm.setPWM(13, 0, pulseWidth(KlappeAuf[3]));//Klappe x   
       Serial3.print("$26");
       Serial3.print("\r");
-
-      
     }
 
     /////Tool Komando/////
 
-    if (cmd == "tool1") {
+    if (cmd == "CB2") {
     if (debug){
-          Serial.println("######Comando - tool1######");
+          Serial.println("######Comando - CB2######");
          Serial.println(cmd);
        }
 
-      Serial3.print(":SE14");
-      Serial3.print("\r");
-      delay(100);
-      Serial2.print("tool1");
+      
+      
+      Serial2.print("tool2");
       Serial2.print("\r");
-      delay(100);
+      delay(4000);
       Serial3.print("$26");
       Serial3.print("\r");
+      delay(500);
+      Serial3.print(":OP05");
+      Serial3.print("\r");
+      
+     
+    }
+    ///// Komando/////
 
-       
+    if (cmd == "CB3") {
+    if (debug){
+          Serial.println("######Comando - tool2######");
+         Serial.println(cmd);
+       }
+      
+         
+
+      Serial2.print("tool3");
+      Serial2.print("\r");
+      delay(5000);
+      Serial3.print(":OP06");
+      Serial3.print("\r");
+
+      
+    }
+    
+    if (cmd == "CB4") {
+    if (debug){
+          Serial.println("######Comando - CB4######");
+         Serial.println(cmd);
+       }
+          
       pwm.setPWM(11, 0, pulseWidth(70));///Klappe 1 auf
       delay(1500);
       pwm.setPWM(0, 0, pulseWidth(160));///Arm 1 hoch
       delay(500);
       pwm.setPWM(1, 0, pulseWidth(140));///Hand Auf
       delay(500);
-      pwm.setPWM(1, 0, pulseWidth(85));///Hand schließen
+      pwm.setPWM(1, 0, pulseWidth(80));///Hand schließen
+      delay(1500);
+      pwm.setPWM(1, 0, pulseWidth(140));///Hand Auf
+      delay(500);
+      pwm.setPWM(1, 0, pulseWidth(80));///Hand schließen
       
-      
-
-      
-
+      Serial3.print("$26");
+      Serial3.print("\r");
       
     }
-    /////Tool Komando/////
 
-    if (cmd == "tool2") {
+    if (cmd == "CB5") {
     if (debug){
-          Serial.println("######Comando - tool2######");
+          Serial.println("######Comando - CB5 Bohren######");
          Serial.println(cmd);
        }
-      
 
       pwm.setPWM(15, 0, pulseWidth(KlappeAuf[5]));//Klappe 1
       delay(1500);
       pwm.setPWM(2, 0, pulseWidth(150));///Arm 2 hoch
-      
-      pwm.setPWM(3, 0, pulseWidth(50));///Schrauber hoch
-      
+      delay(1000);
+      pwm.setPWM(3, 0, pulseWidth(20));///Schrauber hoch
+      delay(500);
+      pwm.setPWM(3, 0, pulseWidth(80));///Schrauber hoch
 
-      Serial2.print("tool2");
-      Serial2.print("\r");
-      delay(100);
+      pwm.setPWM(5, 0, pulseWidth(SchubaAuf[3]));///Schuba S3
+
+      
+      
+      
       Serial3.print("$26");
       Serial3.print("\r");
 
       
     }
+
+     if (cmd == "CB6") {
+    if (debug){
+          Serial.println("######Comando - CB6 Charging######");
+         Serial.println(cmd);
+       }
+
+      pwm.setPWM(13, 0, pulseWidth(KlappeAuf[3]));//Klappe 1
+      delay(1500);
+     
+      Serial3.print("$26");
+      Serial3.print("\r");
+
+      
+    }
+
+    if (cmd == "CB7") {
+    if (debug){
+          Serial.println("######Comando - CB7 Fire ######");
+         Serial.println(cmd);
+       }
+
+          
+      Serial3.print("$26");
+      Serial3.print("\r");
+
+      pwm.setPWM(7, 0, pulseWidth(SchubaZu[1]));///Schuba S1
+      delay(50);
+      pwm.setPWM(6, 0, pulseWidth(SchubaZu[2]));///Schuba S2
+      delay(50);
+       
+    }
+
+    if (cmd == "CB8") {
+    if (debug){
+          Serial.println("######Comando - CB8 Akkucheck ######");
+         Serial.println(cmd);
+       }
+
+          
+      Serial3.print("$26");
+      Serial3.print("\r");
+
+      pwm.setPWM(5, 0, pulseWidth(SchubaZu[3]));///Schuba S3
+      delay(50);
+      pwm.setPWM(4, 0, pulseWidth(SchubaZu[4]));///Schuba S4
+
+      
+    }
+
+    if (cmd == "CB9") {
+    if (debug){
+          Serial.println("######Comando - CB9 Drive ######");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print("$26");
+      Serial3.print("\r");
+      
+    }
+
+    if (cmd == "CB10") {
+    if (debug){
+          Serial.println("######Comando - CB10 Move####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print("$26");
+      Serial3.print("\r");
+      
+    }
+
+    if (cmd == "CB11") {
+    if (debug){
+          Serial.println("######Comando - CB11 Wave####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print(":SE04");
+      Serial3.print("\r");
+
+      Wave();
+      
+    }
     
+    if (cmd == "CB12") {
+    if (debug){
+          Serial.println("######Comando - CB12 Smirk####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print(":SE03");
+      Serial3.print("\r");
 
+      Smirk();
+      
+    }
 
+    if (cmd == "CB13") {
+    if (debug){
+          Serial.println("######Comando - CB13 Disco####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print(":SE07");
+      Serial3.print("\r");
 
-
+      Dance();
+      
+    }
     
+    if (cmd == "CB14") {
+    if (debug){
+          Serial.println("######Comando - CB14 Open All####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print(":OP00");
+      Serial3.print("\r");
+
+      OpenAll();
+      
+    }
+
+    if (cmd == "CB15") {
+    if (debug){
+          Serial.println("######Comando - CB15 Close All####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print(":CL00");
+      Serial3.print("\r");
+
+      CloseAll();
+      
+    }
+
+    if (cmd == "CB16") {
+    if (debug){
+          Serial.println("######Comando - CB16 Sream####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print(":SE15");
+      Serial3.print("\r");
+
+      OpenAll();
+      
+    }
+
+    if (cmd == "CB17") {
+    if (debug){
+          Serial.println("######Comando - CB17 Faint ####");
+         Serial.println(cmd);
+       }
+          
+      Serial3.print(":SE06");
+      Serial3.print("\r");
+
+      Faint();
+      
+    }
 
     /// Filtern ob Dome Komando oder nicht ///
     
